fc7458af4137c5ab4159c182c3d88a01bfcc3c80
MSSim <m.simonenko@g.nsu.ru>
2018-10-23 13:42:30 +0700

    lab0 finish

--- lab0/src/main.c
+++ lab0/src/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+
 #define SIZESTR 60
+#define TOCHNOST 12
+#define  STRALPHABET 12
+#define SIMDOIPOSLETOCHKI 15
 
 long long power(long long a, long long power) {
     long long solve = 1;
@@ -9,19 +13,19 @@ long long power(long long a, long long power) {
     return solve;
 }
 
-long long charToDec(char a) {
+long long char_to_dec(char a) {
     char *alphabet = "aAbBcCdDeEfF";
     if (a >= '0' && a <= '9') {
         return a - '0';
     }
-    for (long long i = 0; i < strlen(alphabet); i++) {
+    for (long long i = 0; i < STRALPHABET; i++) {
         if (a == alphabet[i])
             return 10 + i / 2;
     }
     return -1;
 }
 
-char decToChar(long long a) {
+char dec_to_char(long long a) {
     if ((a > 9) && (a < 16))
         return (char) (a + 'a' - 10);
     if (a >= 0 && a < 10)
@@ -33,29 +37,31 @@ long long b1_to_dec(long long b1, const char *do_tochki) {
     long long solve = 0;
     size_t n = strlen(do_tochki);
     for (long long i = 0; i < n; i++)
-        solve = solve + charToDec(do_tochki[i]) * power(b1, n - i - 1);
+        solve = solve + char_to_dec(do_tochki[i]) * power(b1, n - i - 1);
     return solve;
 }
 
-double b1_to_drob(long long b1, char *posle_tochki) {
+double b1_to_drob(long long b1, const char *posle_tochki) {
     double solve = 0;
-    long long n = strlen(posle_tochki);
+    size_t n = strlen(posle_tochki);
     for (long long i = 0; i < n; i++)
-        solve = solve + (double) (charToDec(posle_tochki[i])) / power(b1, i + 1);
+        solve = solve + (double) (char_to_dec(posle_tochki[i])) / power(b1, i + 1);
     return solve;
 }
+
 
 void print_dec_to_b2(long long b2, long long dec) {
     char ob_solve[SIZESTR] = {0};
     long long i = 0;
+
     while (dec / b2 != 0) {
-        ob_solve[i] = decToChar(dec % b2);
+        ob_solve[i] = dec_to_char(dec % b2);
         dec /= b2;
         i++;
     }
-    ob_solve[i] = decToChar(dec);
+    ob_solve[i] = dec_to_char(dec);
 
-    int len_ob_solve = strlen(ob_solve);
+    size_t len_ob_solve = strlen(ob_solve);
 
     for (i = 0; ob_solve[i] != '\0'; i++) {
 
@@ -64,37 +70,31 @@ void print_dec_to_b2(long long b2, long long dec) {
 }
 
 void print_drob_to_b2(long long b2, double drob) {
-    long long i = 0;
-
     if (drob != 0)
         printf(".");
 
-
-    for (int i=0; i < 20 && drob != 0; i++) {
+    for (int i = 0; i < TOCHNOST && drob != 0; i++) {
         drob *= b2;
-        printf("%c", decToChar((long long) drob));
+        printf("%c", dec_to_char((long long) drob));
         drob -= (long long) drob;
-        i++;
     }
 }
 
-void print_result(long long b1, long long b2,  char *nums) {
-    char do_tochki[15] = {0};
-    char posle_tochki[15] = {0};
-    long long i = 0;
-    for (; nums[i] != '\0'; i++) {
-        if (nums[i] == '.') break;
-        do_tochki[i] = nums[i];
-    }
-    long long len_do_tochki = strlen(do_tochki);
-    for (i++; nums[i] != '\0'; i++) {
-        posle_tochki[i - len_do_tochki - 1] = nums[i];
-    }
 
-    long long dec = b1_to_dec(b1, do_tochki);
-    double drob = b1_to_drob(b1, posle_tochki);
+void print_result(int b1, int b2, char *nums) {
+    char do_tochki[SIMDOIPOSLETOCHKI] = {0};
+    char posle_tochki[SIMDOIPOSLETOCHKI] = {0};
+
+    char *p = strchr(nums, '.');
+    if (p != NULL) *p++ = '\0';
+
+
+    long long dec = b1_to_dec(b1, nums);
     print_dec_to_b2(b2, dec);
-    print_drob_to_b2(b2, drob);
+    if (p != NULL) {
+        double drob = b1_to_drob(b1, p);
+        print_drob_to_b2(b2, drob);
+    }
 }
 
 long long test(long long b1, long long b2, char *nums) {
@@ -102,9 +102,9 @@ long long test(long long b1, long long b2, char *nums) {
         return -1;
     long long s = 0;
     for (long long i = 0; nums[i] != '\0'; i++) {
-        if (charToDec(nums[i]) >= b1 && nums[i] != '.')
+        if (char_to_dec(nums[i]) >= b1 && nums[i] != '.')
             return -1;
-        if (charToDec(nums[i]) == -1 && nums[i] != '.')
+        if (char_to_dec(nums[i]) == -1 && nums[i] != '.')
             return -1;
         if (nums[i] == '.')
             s++;
@@ -117,8 +117,8 @@ long long test(long long b1, long long b2, char *nums) {
 
 int main() {
     int b1, b2;
-    char nums[15];
-    scanf("%d%d%s", &b1, &b2, nums);
+    char nums[15] = {0};
+    scanf("%d%d%15s", &b1, &b2, nums);
 
     if (test(b1, b2, nums) == -1) {
         printf("bad input");
