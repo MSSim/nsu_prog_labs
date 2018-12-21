f9b819df6abf25219c0ec070caead949ec2a110f
MSSim <m.simonenko@g.nsu.ru>
2018-12-22 02:03:14 +0700

    ...

--- lab2/src/main.c
+++ lab2/src/main.c
@@ -2,68 +2,59 @@
 #include <string.h>
 #include <stdlib.h>
 
-static void swap(int p_int[], int tmp, int i, int j) {
-    tmp = p_int[i];
-    p_int[i] = p_int[j];
-    p_int[j] = tmp;
+#define MAX_LEN 10
+
+void print_error(void) {
+    printf("bad input");
+    exit(0);
 }
 
-int test(const char p_char[], const int p_int[], int count[], size_t size) {
-    if (size == 0) {
-        printf("bad input");
-        return 0;
-    }
+static void swap(int *perm_int, int i, int j) {
+    int tmp = perm_int[i];
+    perm_int[i] = perm_int[j];
+    perm_int[j] = tmp;
+}
+
+void test(const char *perm_char, const int *p_int, int *count, size_t size) {
+    if (size == 0) exit(0);
     for (int k = 0; k < size; k++) {
-        if (p_char[k] < '0' || p_char[k] > '9') {
-            printf("bad input");
-            return 0;
-        }
-        if (count[p_int[k]] == 1) {
-            printf("bad input");
-            return 0;
-        }
+        if (perm_char[k] < '0' || perm_char[k] > '9') print_error();
+        if (count[p_int[k]] == 1) print_error();
         count[p_int[k]] = 1;
     }
 }
 
-int dejkstra_algorithm(int *p_int, int size, int tmp, int n) {
+void dijkstra_algorithm(int *perm_int, int size, int n) {
     for (int k = 0; k < n; k++) {
         int i = size - 2;
         int j = size - 1;
-        while (i >= 0 && p_int[i] >= p_int[i + 1]) {
-            i--;
-        }
-        if (i == -1)
-            return 0;
-        while (j > i && p_int[j] <= p_int[i]) {
-            j--;
-        }
-        swap(p_int, tmp, i, j);
+        while (i >= 0 && perm_int[i] >= perm_int[i + 1]) i--;
+        if (i == -1) break;
+        while (j > i && perm_int[j] <= perm_int[i]) j--;
+        swap(perm_int, i, j);
         for (int a = i + 1; a < size - 1; a++) {
-            if (p_int[a] > p_int[a + 1]) {
-                swap(p_int, tmp, a, a + 1);
+            if (perm_int[a] > perm_int[a + 1]) {
+                swap(perm_int, a, a + 1);
             }
         }
         for (int l = 0; l < size; l++) {
-            printf("%d", p_int[l]);
+            printf("%d", perm_int[l]);
         }
         printf("\n");
     }
 }
 
 int main() {
-    char p_char[11];
-    int count[10] = {0};
-    int p_int[11] = {0};
-    int n;
-    int tmp;
-    scanf("%s", p_char);
+    char perm_char[MAX_LEN + 1];
+    int count[MAX_LEN] = {0};
+    int perm_int[MAX_LEN] = {0};
+    int n = 0;
+    scanf("%10s", perm_char);
     scanf("%d", &n);
-    size_t size = strlen(p_char);
+    size_t size = strlen(perm_char);
     for (int i = 0; i < size; i++) {
-        p_int[i] = p_char[i] - '0';
+        perm_int[i] = perm_char[i] - '0';
     }
-    if (test(p_char, p_int, count, size) == 0)
-        return 0;
-    dejkstra_algorithm(p_int, size, tmp, n);
+    test(perm_char, perm_int, count, size);
+    dijkstra_algorithm(perm_int, size, n);
 }
